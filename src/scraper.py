b3693e0e3e889b63b193490dd9ebd5547dd8c89b
adiswami14 <adithya1431@gmail.com>
2021-09-12 03:00:25 -0500

    Create auxiliary file for teams

--- src/scraper.py
+++ src/scraper.py
@@ -7,7 +7,7 @@ headers = {'User-Agent':
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
 }
 
-years = [i for i in range(2013, 2021)]
+years = [i for i in range(2014, 2021)]
 weeks = [i for i in range(1, 20)]
 
 
@@ -20,8 +20,8 @@ def scrape():
 def standardize_name(name):
     return name.replace(' ', '-').lower()
 
-# Extracts tangible data from HTML source code
-def extract_data(df):
+# Extracts tangible data for pollster names from HTML source code
+def extract_name_data(df):
     url = 'https://collegepolltracker.com/basketball/pollsters/'
     for year in years:
         i = 0
@@ -37,7 +37,44 @@ def extract_data(df):
 
     return df
 
+def extract_team_data(pollster_df, team_df):
+    url = 'https://collegepolltracker.com/basketball/pollster/'
+    for year in years: # represents a single year
+        teams = []
+        for i in range(0, 67):
+            for week in weeks:
+                i=0
+                week_var = week
+
+                if week_var == 0:
+                    week_var = "pre-season"
+                elif week_var == 19:
+                    week_var = "final-rankings"
+
+                page = requests.get(url + str(names_df.at[i, year])+'/'+str(year)+'/'+str(week_var), headers = headers)
+                soup = BeautifulSoup(page.content, 'html.parser')
+                results = soup.find_all("span", {"class": "teamName"})
+
+                for result in results:
+                    if result.text not in teams:
+                        if year is years[0]:
+                            team_df = team_df.append({year: standardize_name(result.text)}, ignore_index=True)
+                        else: 
+                            team_df.loc[i, year] = standardize_name(result.text)
+                        i+=1
+                        teams.append(result.text)
+
+    return team_df
+
+
+# Create auxiliary files for all pollster names
 names_df = pd.DataFrame(columns=years)
-names_df = extract_data(names_df)
-print(names_df)
+names_df = extract_name_data(names_df)
 names_df.to_csv('names.csv')
+
+# Create auxiliary files for all teams ballotted at least once
+teams_df = pd.DataFrame(columns=years)
+teams_df = extract_team_data(names_df, teams_df)
+teams_df.to_csv('teams.csv')
+
+
